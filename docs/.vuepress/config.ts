45dbcdc0a8244853b7ed0a68f14c78b048f947de
AFXR Hikari <theaurora@outlook.com>
2023-07-30 14:48:02 +0800

    comments

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -265,11 +265,11 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
       {
         choosen: 'gitalk',
         options: {
-          clientID: 'a6e1355287947096b88b',
-          clientSecret: 'f0e77d070fabfcd5af95bebb82b2d574d7248d71',
-          repo: 'blog-gitalk-comment', // GitHub 仓库
-          owner: 'xugaoyi', // GitHub仓库所有者
-          admin: ['xugaoyi'], // 对仓库有写权限的人
+          clientID: '5f296b1f9720d2cc2e89',
+          clientSecret: 'c9729f78869dfcdb0ad7555407ba80dcec207913',
+          repo: 'gitalk-comments', // GitHub 仓库
+          owner: 'AFXR17light', // GitHub仓库所有者
+          admin: ['AFXR17light'], // 对仓库有写权限的人
           // distractionFreeMode: true,
           pagerDirection: 'last', // 'first'正序 | 'last'倒序
           id: '<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>', //  页面的唯一标识,长度不能超过50
